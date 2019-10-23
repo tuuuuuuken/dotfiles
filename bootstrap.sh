df51c70edf3d9d5734d5c750744d88230fed4499
kento tanaka <aaaaa.knt1112@gmail.com>
2019-10-23 23:04:23 +0900

    test

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,6 +26,8 @@ if ! command -v brew >/dev/null 2>&1; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   echo
 fi
+
+
 brew bundle
 echo
 
